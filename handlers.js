936839be3875ce52d335102763ffe0896f39c121
Pierre Trollé <ptrolle@octo.com>
2016-10-08 14:42:02 +0200

    extract handlers from routes

---
+++ handlers.js
@@ -0,0 +1,12 @@
+module.exports = {
+    helloWorld (request, reply) {
+        reply('Hello world!');
+    },
+    getAllPokemons(request, reply) {
+        const data = require("./data.json");
+        reply(data);
+    },
+    getDays(request, reply) {
+        reply([]);
+    }
+}
