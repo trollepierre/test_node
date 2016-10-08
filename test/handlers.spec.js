f4add726ac16d397b3398b1a3253e6b29756d22e
Pierre Trollé <ptrolle@octo.com>
2016-10-08 14:49:14 +0200

    feat: ajout de tests sur le handler

---
+++ test/handlers.spec.js
@@ -0,0 +1,47 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+const handlers = require('../handlers');
+chai.use(require('sinon-chai'));
+
+describe('Handlers', () => {
+
+  describe('.helloWorld()', () => {
+    it('should reply with Hello World !', () => {
+      // Given
+      const mySpy = sinon.spy();
+
+      // When
+      handlers.helloWorld({}, mySpy);
+
+      // Then
+      expect(mySpy).to.have.been.calledWith('Hello world!');
+    });
+  });
+
+  describe('.getAllPokemons()', () => {
+    it('should get all pokemons !', () => {
+      // Given
+      const mySpy = sinon.spy();
+
+      // When
+      handlers.getAllPokemons({}, mySpy);
+
+      // Then
+      expect(mySpy).to.have.been.calledWith(require('../data.json'));
+    });
+  });
+
+  describe('.getDays()', () => {
+    it('should get days !', () => {
+    // Given
+    const mySpy = sinon.spy();
+
+    // When
+    handlers.getDays({}, mySpy);
+
+    // Then
+    expect(mySpy).to.have.been.calledWith([]);
+  });
+});
+});
